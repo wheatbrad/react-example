119c260f81fdb54fc588d0f3450b93ede6ec72e6
Brad Runnels <runnels.brad@gmail.com>
2023-05-26 14:06:05 -0400

    transition to vitejs and rollup

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from 'react';
+import LabeledTextInput from './components/LabeledTextInput';
+
+function App() {
+  const data = useRef(document.getElementById('FIELD_555'));
+  const [state, setState] = useState({});
+  const focus = useRef(null);
+  const reset = useRef(0);
+
+  const handleUpdate = (key, value) => {
+    if (value === '') return;
+
+    setState(prevState => ({
+      ...prevState,
+      [key]: value
+    }));
+  };
+
+  const handleReset = () => {
+    setState({});
+    reset.current++;
+  };
+
+  useEffect(() => {
+    data.current.value = JSON.stringify(state);
+  }, [state]);
+
+
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <LabeledTextInput
+        ref={focus}
+        label='Name'
+        handleUpdate={handleUpdate}
+        reset={reset} />
+      <LabeledTextInput label='Title' handleUpdate={handleUpdate} reset={reset} />
+      <LabeledTextInput label='Company' handleUpdate={handleUpdate} reset={reset} />
+      <button onClick={() => handleReset()}>
+        Clear Data
+      </button>
+    </form>
+  );
+}
+
+export default App;
